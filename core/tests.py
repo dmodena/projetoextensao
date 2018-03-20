5a7c82f18e00771d6c2418e8f4eb6ed01cfb75c6
Douglas Modena <d.modena@live.com>
2018-03-20 16:00:41 -0300

    Tests added.

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+from core.templatetags import custom_filters
 
-# Create your tests here.
+class TimeMasksTests(TestCase):
+    def test_date_mask(self):
+        test_date = datetime.date(2017, 11, 21)
+        self.assertEqual(custom_filters.date_mask(test_date), "21/11/2017")
+
+    def test_datefull_mask(self):
+        test_date = datetime.date(2017, 11, 21)
+        self.assertEqual(custom_filters.datefull_mask(test_date), "21 de Novembro de 2017")
+
+    def test_datetime_mask(self):
+        test_date = datetime.datetime(2017, 11, 21, 22, 45, 00, 0, tzinfo=None)
+        self.assertEqual(custom_filters.datetime_mask(test_date), "21/11/2017 22:45")
